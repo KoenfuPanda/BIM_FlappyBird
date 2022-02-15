4136844b2baa7ed71d64d572a19d1ed6d571acd1
Rica-FF <99659612+Rica-FF@users.noreply.github.com>
2022-02-15 17:01:01 +0100

    Proto 7 +11 + Cannon0.1

---
+++ Assets/Scripts/SpawnCannonBall.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCannonBall : MonoBehaviour
+{
+    [SerializeField] private GameObject cannonBall;
+
+    void Start()
+    {
+        Instantiate(cannonBall, gameObject.transform.position, Quaternion.Euler(0, 0, 0));
+    }
+}
