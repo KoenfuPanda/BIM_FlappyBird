9017f71d7179d1565ab2c5e1ab2ed9d0ead0f4dd
KoenfuPanda <koen.laureys@hotmail.be>
2022-02-15 17:36:08 +0100

    added pseudo code for myself

--- Assets/Scripts/MoveDirection.cs
+++ Assets/Scripts/MoveDirection.cs
@@ -16,6 +16,15 @@ public class MoveDirection : MonoBehaviour
         // Move root to the right
 
         transform.Translate(new Vector3( speed * Time.deltaTime, 0 ,0 ));
+
+        // -- FOR FOLLOW BIM EVERYWHERE --
+        // 1) make it so that the logic in here is only applied to the camera, not Bim
+        // 2) make followfinger logic also be applied to x-axis mouse position
+        // NOTES
+        // - make Bim turn 180 degrees depending on the movement 5 not needed, but perhaps)
+        // - proppelor ?!
+        // - vertical matrass adjustment needed, add a constant speed forward (equal to cam moving speed)
+
     }
 
     IEnumerator DelayStart()
