75d42d9f89ed6e61b0c3858a1ab0a3447d7c183e
KoenfuPanda <koen.laureys@hotmail.be>
2022-02-16 15:09:25 +0100

    2 pickup particles + sound effect

--- Assets/Scripts/Feathers.cs
+++ Assets/Scripts/Feathers.cs
@@ -12,12 +12,44 @@ public class Feathers : MonoBehaviour
     private float timeToReachTarget = 0.5f;
     private GameObject player;
 
+    private SpriteRenderer _spriteRenderer;
+    private CircleCollider2D _collider;
+
+    private AudioSource _audioSource;
+
+    [SerializeField]
+    private GameObject _particleSystemPickup, _particleSystemGlow;
+
+    [SerializeField]
+    private List<AudioClip> _possibleSounds;
+
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _collider = GetComponent<CircleCollider2D>();
+        _audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             FeatherScore.feathers++;
-            Destroy(gameObject);
+
+            Destroy(_particleSystemGlow);
+            Destroy(_collider);
+            Destroy(_spriteRenderer);
+
+            // play sound effect
+            if (_possibleSounds.Count > 0) ;
+            {
+                _audioSource.PlayOneShot(_possibleSounds[Random.Range(0, _possibleSounds.Count - 1)]);
+            }
+            // instantiate particle
+            Instantiate(_particleSystemPickup, transform.position, Quaternion.identity);
+
+            Destroy(gameObject, 2f);
         }
     }
 
---
+++ Assets/Scripts/ParticleSelfDestruct.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSelfDestruct : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(this.gameObject, 2f);
+    }
+}
