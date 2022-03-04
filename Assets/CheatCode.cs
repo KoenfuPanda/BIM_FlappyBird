1dde4ea1742d038a60c4df7aa8e5b1fff1f198ab
Rica-FF <99659612+Rica-FF@users.noreply.github.com>
2022-03-04 09:12:44 +0100

    music and cheat code

---
+++ Assets/CheatCode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatCode : MonoBehaviour
+{
+    public GameObject _gameInstance;
+    public int _timesPressed = 0;
+
+    private void Start()
+    {
+        _gameInstance = GameObject.Find("GameInstance(Clone)");       
+    }
+    public void UnlockEverything()
+    {
+        _timesPressed++;
+
+        if (_timesPressed > 10)
+        {
+            _gameInstance.GetComponent<GameInstance>().GameState = 6;
+            _gameInstance.GetComponent<GameInstance>().SetLevelButtons();
+        }
+    }
+}
+
