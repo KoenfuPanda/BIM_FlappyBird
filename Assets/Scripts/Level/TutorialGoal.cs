7a47d26e5fdb91332524a6f0beaf70c5c1500031
Koen <laureys.koen@hotmail.be>
2022-03-07 17:48:07 +0100

    matras tutorial added

--- Assets/Scripts/Level/TutorialGoal.cs
+++ Assets/Scripts/Level/TutorialGoal.cs
@@ -8,12 +8,15 @@ public class TutorialGoal : MonoBehaviour
     private TutorialLogic _tutorialTrigger;
 
     [SerializeField]
-    private bool _loseControl;
+    private bool _loseControl, _freezeWithoutContinue;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        _tutorialTrigger.ContinueTheGame();
-
+        if (_tutorialTrigger != null && _freezeWithoutContinue == false)
+        {
+            _tutorialTrigger.ContinueTheGame();
+        }
+        
         if (_loseControl == true)
         {
             _tutorialTrigger.FollowFingerScript.TurnOffControl(1000, true, false, true);
