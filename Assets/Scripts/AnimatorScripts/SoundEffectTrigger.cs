8bc756ae8abb5286ac376888ba283c9434c77e3a
Koen <laureys.koen@hotmail.be>
2022-03-10 14:01:00 +0100

    Score screen sound implementations part 1

---
+++ Assets/Scripts/AnimatorScripts/SoundEffectTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private List<AudioClip> _soundEffects = new List<AudioClip>();
+
+    private AudioSource _audioSource;
+
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+
+    private void PlaySoundEffect(int index)
+    {
+        _audioSource.PlayOneShot(_soundEffects.ToArray()[index]);
+    }
+}
