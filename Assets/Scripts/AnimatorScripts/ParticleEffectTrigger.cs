63dfdedfc8b17ff1e51065ce4f1e6ed11cea573a
Koen <laureys.koen@hotmail.be>
2022-03-11 16:27:26 +0100

    special pieces placement, extra work score

---
+++ Assets/Scripts/AnimatorScripts/ParticleEffectTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEffectTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _requiredParticleParent;
+
+    public void SetParticleParentActive()
+    {
+        _requiredParticleParent.SetActive(true);
+    }
+}
