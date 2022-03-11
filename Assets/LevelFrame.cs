afced85981a53d7b46ee4e5626d09d1db8b6a057
Rica-FF <ricardo.gomes@fabriquefantastique.be>
2022-03-11 10:45:33 +0100

    New Menu

---
+++ Assets/LevelFrame.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelFrame : MonoBehaviour
+{
+    public List<Image> Stars = new List<Image>();
+    public List<Image> EmblemPieces = new List<Image>();
+}
---
+++ Assets/LevelFramesManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelFramesManager : MonoBehaviour
+{
+    public List<LevelFrame> Frames = new List<LevelFrame>();
+
+    void Start()
+    {
+        StartCoroutine(DelayStart());
+    }
+
+    IEnumerator DelayStart()
+    {
+        yield return new WaitForSeconds(1);
+
+        if(GameInstance.CollectedEggs[0,0])
+        {
+            Frames[0].EmblemPieces[0].enabled = true;
+        }
+    }
+}
