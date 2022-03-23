a941a849a4ec6509690a1e09c38501cadd559e29
KoenfuPanda <koen.laureys@hotmail.be>
2022-03-23 10:45:04 +0100

    work on level_04 + testing shape script

---
+++ Assets/Prefabs/testingShape.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class testingShape : MonoBehaviour
+{
+    public SpriteShapeController _shaper;
+
+    private void Start()
+    {
+        _shaper = GetComponent<SpriteShapeController>();
+
+    }
+
+    private void Update()
+    {
+        _shaper.spline.GetPosition(0);
+        _shaper.spline.GetPosition(1);
+
+
+    }
+}
