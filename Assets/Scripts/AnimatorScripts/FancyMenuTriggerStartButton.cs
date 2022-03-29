037874634b1d6bf5fcd423873185630ed0c28c24
Koen <koen.laureys@hotmail.be>
2022-03-29 11:55:59 +0200

    Work on fancier menu

---
+++ Assets/Scripts/AnimatorScripts/FancyMenuTriggerStartButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FancyMenuTriggerStartButton : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _startCanvasAnimator;
+
+
+
+
+    private void StartShowingButton()
+    {
+        _startCanvasAnimator.enabled = true;
+    }
+}
